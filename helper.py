a4cf095618a02df1adf3c80d4466a0ce9fe85e99
Mathu_Gopalan <mathu.bhalan@gmail.com>
2018-10-01 15:55:06 +0100

    skull

---
+++ helper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 15:30:18 2018
+
+@author: Mathu_Gopalan
+"""
+
+#place configurations in config.yaml
+
+#read the configurations
+
+#pull data from GA
